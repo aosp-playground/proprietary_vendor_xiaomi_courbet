8d7b67eb73f2196134108ab84b4d710bbe4c8b3f
basamaryan <basam.aryan@gmail.com>
2023-05-08 19:36:39 +0000

    courbet: Import decommonized liboemcrypto
    Change-Id: I9ff41028c264d27d74452ba02cbc7d4ed9272dbd

--- courbet-vendor.mk
+++ courbet-vendor.mk
@@ -330,6 +330,7 @@ PRODUCT_PACKAGES += \
     libmulticam_image_optical_zoom \
     libmulticam_video_optical_zoom \
     libnotifyaudiohal \
+    liboemcrypto \
     librelight_only \
     libremosaiclib \
     libsdk_sr \
